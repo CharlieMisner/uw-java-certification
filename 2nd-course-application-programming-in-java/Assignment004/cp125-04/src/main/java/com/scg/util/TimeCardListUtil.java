e839b58ebb85b48629e52ef403fa69fb8ab2c994
Charlie <charliemisner@gmail.com>
2019-02-11 13:02:57 -0800

    Created assignment 4 classes

---
+++ 2nd-course-application-programming-in-java/Assignment004/cp125-04/src/main/java/com/scg/util/TimeCardListUtil.java
@@ -0,0 +1,25 @@
+package com.scg.util;
+
+import com.scg.domain.Consultant;
+import com.scg.domain.TimeCard;
+
+import java.util.List;
+
+public class TimeCardListUtil {
+
+//    public static List<TimeCard> getTimeCardsForConsultant(List<TimeCard> timeCards, Consultant consultant){
+//
+//    }
+//
+//    public static List<TimeCard>getTimeCardsForDateRange(List<TimeCard> timeCards, DateRange dateRange){
+//
+//    }
+
+    public static void sortByConsultantName(List<TimeCard> timeCards){
+
+    }
+
+    public static void	sortByStartDate(List<TimeCard> timeCards){
+
+    }
+}
