dc7cb3831bb6d23f54ebbc56d3a9bfde04419296
chq-charliem <charlie.misner@expeditors.com>
2019-02-19 13:30:58 -0800

    started comparator

--- 2nd-course-application-programming-in-java/Assignment004/cp125-04/src/main/java/com/scg/util/TimeCardConsultantComparator.java
+++ 2nd-course-application-programming-in-java/Assignment004/cp125-04/src/main/java/com/scg/util/TimeCardConsultantComparator.java
@@ -2,7 +2,27 @@ package com.scg.util;
 
 import com.scg.domain.TimeCard;
 
+import java.time.ZoneId;
 import java.util.Comparator;
 
-public class TimeCardConsultantComparator {
+public class TimeCardConsultantComparator implements Comparator<TimeCard> {
+
+    public int compare(TimeCard firstTimeCard, TimeCard secondTimeCard){
+
+        int diff = 0;
+
+        String firstConsultantName = firstTimeCard.getConsultant().toString();
+        String secondConsultantName = secondTimeCard.getConsultant().toString();
+        long firstBeginningDateMillis = firstTimeCard.getWeekStartingDay().atStartOfDay()
+                .atZone(ZoneId.of("America/Los_Angeles")).toInstant().toEpochMilli();
+        long secondBeginningDateMillis = secondTimeCard.getWeekStartingDay().atStartOfDay()
+                .atZone(ZoneId.of("America/Los_Angeles")).toInstant().toEpochMilli();
+        int firstTotalBillableHours = firstTimeCard.getTotalBillableHours();
+        int secondTotalBillableHours = secondTimeCard.getTotalBillableHours();
+        int firstNonBillableHours = firstTimeCard.getTotalNonBillableHours();
+        int secondNonBillableHours = secondTimeCard.getTotalNonBillableHours();
+
+        return 0;
+    }
+
 }
